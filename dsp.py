3e28407651d63e5893429a55a605371a9729a36e
omarelwaliely <77898546+omarelwaliely@users.noreply.github.com>
2023-10-11 20:37:27 +0300

    Update dsp.py
    editede

--- dsp.py
+++ dsp.py
@@ -1,25 +1,42 @@
 import cv2
-import numpy as np
-from skimage.measure import compare_ssim as ssim
+from skimage.metrics import structural_similarity as ssim
 
 cap = cv2.VideoCapture('Video.mp4')
- 
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
-firstret, firstframe = cap.read()
-count =0
-if firstret:
-  while(cap.isOpened()):
-    count+=1
-    ret, frame = cap.read()
-    if ret == True:
-      if firstframe == frame:
-        simlarityIndex = ssim(firstframe, frame)
-        print(f"count:{count}, similarity: {simlarityIndex}")
-      #cv2.imshow('Frame',frame)
-      if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-    else: 
-      break
+
+if not cap.isOpened():
+    print("Error opening video stream or file")
+    exit()
+
+frame_rate = cap.get(cv2.CAP_PROP_FPS)  # Get the frame rate of the video
+seconds = 1  # Number of seconds to check for repeated frames
+frame_count = int(frame_rate * seconds)  # Calculate the number of frames in 'seconds'
+
+ret, firstframe = cap.read()
+
+if ret:
+    count = 1  # Initialize count at 1 for the first frame
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret:
+            similarity_index, _ = ssim(firstframe, frame, full=True)
+
+            if similarity_index == 1.0:
+                print(f"count: {count}, similarity: {similarity_index}")
+
+            count += 1
+
+            # If the count reaches the number of frames in 'seconds', reset the first frame
+            if count > frame_count:
+                count = 1
+                firstframe = frame.copy()
+        else:
+            break
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+else:
+    print("Error reading the first frame.")
+
 cap.release()
 cv2.destroyAllWindows()
